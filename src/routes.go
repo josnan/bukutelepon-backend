75c70a82f3934e18c9201bdad36d7ffedae48465
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-03-24 20:05:08 +0700

    [Dev] Create route to get all contacts

--- src/routes.go
+++ src/routes.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"./models"
+	"github.com/gin-gonic/gin"
+)
 
 type kontak struct {
 	nama    string
@@ -9,15 +14,15 @@ type kontak struct {
 
 // Routes of all Routes
 func Routes(r *gin.Engine) {
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"status": "ok",
-			"data": []gin.H{
-				gin.H{
-					"nama":    "Hadi Hidayat Hammurabi",
-					"telepon": "082140320292",
-				},
-			},
-		})
-	})
+	r.GET("/", index)
+}
+
+func index(c *gin.Context) {
+	contacts, err := models.ContactModelAll()
+
+	if err != nil {
+		log.Fatal("Model Error Get All Contacts: ", err)
+	}
+
+	c.JSON(200, contacts)
 }
