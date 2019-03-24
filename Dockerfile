16f64cb426c7e9c4d6e015f38b920aa1616913bf
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-03-25 00:21:40 +0700

    [Dev] Add CORS

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ADD . /app/
 WORKDIR /app
 
 RUN go get -u github.com/gin-gonic/gin
+RUN go get github.com/gin-contrib/cors
 RUN go get -u github.com/lib/pq
 
 RUN go build src/*.go
--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 
 	"./models"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -14,7 +15,12 @@ func main() {
 	models.Init(fmt.Sprintf("postgresql://%s@%s:26257/%s?sslmode=disable", dbconfig.user, dbconfig.host, dbconfig.database))
 
 	r := gin.Default()
+	r.Use(corsMiddleware())
 	Routes(r)
 
 	r.Run()
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return cors.Default()
 }
