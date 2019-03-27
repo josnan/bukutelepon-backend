9c4da44d1a139655b5944858454976c6d5fa7084
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-03-27 14:27:54 +0700

    [Dev] Create insert and delete endpoints

--- src/models/contact.go
+++ src/models/contact.go
@@ -1,5 +1,7 @@
 package models
 
+import "fmt"
+
 // Contact structure
 type Contact struct {
 	ID        int    `json:"id"`
@@ -34,4 +36,46 @@ func ContactModelAll() ([]*Contact, error) {
 	}
 
 	return contacts, nil
+}
+
+// ContactModelByID to get a contact by id
+func ContactModelByID(id string) (*Contact, error) {
+	rows, err := db.Query(fmt.Sprintf("SELECT * FROM contacts WHERE id=%s", id))
+
+	if err != nil {
+		return nil, err
+	}
+
+	contact := new(Contact)
+	for rows.Next() {
+		err := rows.Scan(&contact.ID, &contact.Name, &contact.Phone, &contact.CreatedAt, &contact.UpdatedAt)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return contact, nil
+}
+
+// ContactModelCreate to create a contact
+func ContactModelCreate(contact *Contact) (bool, error) {
+	_, err := db.Query(fmt.Sprintf("INSERT INTO contacts(name, phone) VALUES ('%s', '%s')", contact.Name, contact.Phone))
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+// ContactModelDelete to delete a contact by ID
+func ContactModelDelete(id string) (bool, error) {
+
+	_, err := db.Query(fmt.Sprintf("DELETE FROM contacts WHERE id=%s", id))
+
+	if err != nil {
+		return false, nil
+	}
+
+	return true, nil
 }
--- src/routes.go
+++ src/routes.go
@@ -7,14 +7,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type kontak struct {
-	nama    string
-	telepon string
+// Response structure
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
 // Routes of all Routes
 func Routes(r *gin.Engine) {
 	r.GET("/", index)
+	r.POST("/", insertContact)
+	r.DELETE("/:id", deleteContact)
+	r.PUT("/:id", updateContact)
 }
 
 func index(c *gin.Context) {
@@ -22,7 +26,91 @@ func index(c *gin.Context) {
 
 	if err != nil {
 		log.Fatal("Model Error Get All Contacts: ", err)
+		response := &Response{
+			Status:  "FAIL",
+			Message: "Error while getting contacts.",
+		}
+		c.JSON(200, response)
+	} else {
+		c.JSON(200, contacts)
+	}
+}
+
+func insertContact(c *gin.Context) {
+	var newcontact *models.Contact
+	var response *Response
+	err := c.ShouldBindJSON(&newcontact)
+
+	if err != nil {
+		log.Fatal("Create Contact Error: ", err)
+		response = &Response{
+			Status:  "FAIL",
+			Message: "Error while inserting contact.",
+		}
 	}
 
-	c.JSON(200, contacts)
+	success, err := models.ContactModelCreate(newcontact)
+
+	if err != nil {
+		log.Fatal("Create Contact Error: ", err)
+		response = &Response{
+			Status:  "FAIL",
+			Message: "Error while inserting contact.",
+		}
+	}
+
+	if success {
+
+	}
+
+	response = &Response{
+		Status:  "OK",
+		Message: "New contact has been saved.",
+	}
+
+	c.JSON(200, response)
 }
+
+func deleteContact(c *gin.Context) {
+	var response *Response
+	id := c.Param("id")
+
+	_, err := models.ContactModelDelete(id)
+
+	if err != nil {
+		log.Fatal("Delete Contact Error: ", err)
+		response = &Response{
+			Status:  "FAIL",
+			Message: "Error while deleting contact.",
+		}
+	}
+
+	response = &Response{
+		Status:  "OK",
+		Message: "Contact has been deleted.",
+	}
+
+	c.JSON(200, response)
+}
+
+// func updateContact(c *gin.Context) {
+// 	var response *Response
+// 	var newcontact *models.Contact
+// 	id := c.Param("id")
+
+// 	contact, err := models.ContactModelByID(id)
+
+// 	if err != nil || contact.ID == 0 {
+// 		response = &Response{
+// 			Status:  "FAIL",
+// 			Message: "Contact not found.",
+// 		}
+// 		c.JSON(404, response)
+// 	} else {
+
+// 		_, err := models.ContactModelUpdate(id, )
+
+// 		c.JSON(200, contact)
+// 	}
+
+// }
